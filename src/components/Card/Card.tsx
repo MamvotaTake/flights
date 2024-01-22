fd78a74e17c350cdff758d5745235add4e96a566
Takesure Mamvota <tooyoungmamvota@gmail.com>
2024-01-22 16:32:14 +0700

    Card added

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,41 @@
 import './Card.scss'
 export function Card() {
     return (
-        <>Card</>
+        <div className="Card">
+            <div className="Card__header">
+                <p className="Card__header-price">13 400 Р</p>
+                <img src="../../../public/s7_logo.png" alt="logo" className="Card__header-logo-image"/>
+            </div>
+            <div className="Card__body">
+                <div className="Card__body-details">
+                    <p>
+                        <span>MOW - HKT</span>
+                        10:45 - 11:35
+                    </p>
+                    <p>
+                        <span>В ПУТИ</span>
+                        21ч 15м
+                    </p>
+                    <p>
+                        <span>2 ПЕРЕСАДКИ</span>
+                        HKG, JNB
+                    </p>
+                </div>
+                <div className="Card__body-details">
+                    <p>
+                        <span>MOW - HKT</span>
+                        10:45 - 11:35
+                    </p>
+                    <p>
+                        <span>В ПУТИ</span>
+                        21ч 15м
+                    </p>
+                    <p>
+                        <span>2 ПЕРЕСАДКИ</span>
+                        HKG, JNB
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,13 @@
 import './Main.scss';
+import {Card} from "../Card";
 export function Main() {
     return (
-        <div className="main">Main</div>
+        <div className="main">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
     );
 }
