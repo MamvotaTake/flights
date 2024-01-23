855450bffd01be86e4d27fd6f58a064f8927c865
Takesure Mamvota <tooyoungmamvota@gmail.com>
2024-01-23 23:50:29 +0700

    added navigation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,10 @@
 import './Main.scss';
 import {Card} from "../Card";
+import {Navigation} from "../Navigation";
 export function Main() {
     return (
         <div className="main">
+            <Navigation/>
             <Card />
             <Card />
             <Card />
